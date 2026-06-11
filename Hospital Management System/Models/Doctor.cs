e99080c952c0cd49b783d9333c56125523cee209
Sheika <sheika02x2@gmail.com>
2026-06-11 11:14:11 +0400

    updated Doctor class

--- Hospital Management System/Models/Doctor.cs
+++ Hospital Management System/Models/Doctor.cs
@@ -12,5 +12,7 @@ namespace Hospital_Management_System.Models
         public string doctorName { get; set; }
         public string doctorSpecialization { get; set; }
         public string doctorPhone { get; set; }
+        public string doctorEmail { get; set; }
+        public decimal consultationFee { get; set; }
     }
 }
