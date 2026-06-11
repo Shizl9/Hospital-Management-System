e320fd417e0eade97545e50a5fb68f963c330436
Sheika <sheika02x2@gmail.com>
2026-06-11 11:46:57 +0400

    created new object named main context empty

--- Hospital Management System/obj/Debug/net8.0/Hospital Management System.AssemblyInfo.cs
+++ Hospital Management System/obj/Debug/net8.0/Hospital Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac1b6bd0d612ee59977ea054d0f03d68dd34ff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70bdd7059275b9a238afcb37fe2c6b682ba79198")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
