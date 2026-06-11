b93296d88b51dec0e38226cf88f373d0f787d857
Sheika <sheika02x2@gmail.com>
2026-06-11 11:17:12 +0400

    add Appointment class

--- Hospital Management System/Models/Appointment.cs
+++ Hospital Management System/Models/Appointment.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Hospital_Management_System.Models
 {
-    internal class Appointment
+    public class Appointment
     {
+        public int appointmentId { get; set; }
+        public int patientId { get; set; }
+        public int doctorId { get; set; }
+        public string appointmentDate { get; set; }
+        public string appointmentTime { get; set; }
+        public string status { get; set; }
     }
 }
--- Hospital Management System/obj/Debug/net8.0/Hospital Management System.AssemblyInfo.cs
+++ Hospital Management System/obj/Debug/net8.0/Hospital Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb44c1838007e7a8c43f484a9991e2fd8bc3eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e99080c952c0cd49b783d9333c56125523cee209")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
