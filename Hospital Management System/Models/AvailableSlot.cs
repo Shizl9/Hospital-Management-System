7f79d496107a70f15248f42ad8cb848ef5f7e86e
Sheika <sheika02x2@gmail.com>
2026-06-11 11:23:58 +0400

    add AvailableSlot class

--- Hospital Management System/Models/AvailableSlot.cs
+++ Hospital Management System/Models/AvailableSlot.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Hospital_Management_System.Models
 {
-    internal class AvailableSlot
+    public class AvailableSlot
     {
+        public int slotId { get; set; }
+        public int doctorId { get; set; }
+        public string slotDate { get; set; }
+        public string slotTime { get; set; } 
+        public bool isBooked { get; set; }
     }
 }
